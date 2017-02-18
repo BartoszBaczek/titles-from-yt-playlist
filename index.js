79dbae8486dabb9405a84cb7db2c0109798636f8
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-02-18 15:49:10 +0100

    moved index.js to root folder

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-require('./app/index');
+const playlist = require("./app/playlist");
+const express = require("express");
+
+const port = 3000;
+
+let app = express();
+
+app.get('/', (req, res) => {
+    playlist.getData((err, data) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+let server = app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
+
+
+
+
+
