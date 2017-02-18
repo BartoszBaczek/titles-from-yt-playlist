75317a087208fab0627b4430a292c6f80ce32cda
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-02-18 11:43:30 +0100

    added basic express server

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
+const express = require("express");
 const yt = require("youtube-api");
+const port = 3000;
+
+
+let app = express();
+app.get('/', (req, res) => {
+    res.send('here we go');
+});
+
+let server = app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
