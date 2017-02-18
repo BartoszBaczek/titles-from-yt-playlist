cc6e0bc11cf3958f380a8bf684d1207abfadf328
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-02-18 10:41:37 +0100

    added reading text file sample. To be removed soon to

--- app/index.js
+++ app/index.js
@@ -1,6 +1,24 @@
 const calc = require("./calc");
+const fs = require("fs");
+
+
+
+// easy
 
 const numbersToAdd = [1, 2, 3, 4];
 
 const result = calc.sum(numbersToAdd);
 console.log(`result is ${result}`)
+
+
+// read file
+
+let content;
+
+try {
+    content = fs.readFileSync(__dirname + '/commons/file.txt', 'utf-8');
+} catch (er) {
+    console.log(er);
+}
+
+console.log(`hehe udalo sie ${content}`);
